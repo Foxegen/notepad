fe73a9c9b39433b1cd4046d27ffc8254ea943b8e
Foxegen <foxegen55@gmail.com>
2020-07-22 13:44:00 +0300

    Notepad 1.0

--- post.rb
+++ post.rb
@@ -1,5 +1,13 @@
 class Post
 
+  def self.post_types
+    [Memo, Link, Task]
+  end
+
+  def self.create(type_index)
+    return post_types[type_index].new
+  end
+
   def initialize
     @created_at = Time.now
     @text = nil
@@ -30,4 +38,5 @@ class Post
 
     return current_path + "/" + file_name
   end
+
 end
