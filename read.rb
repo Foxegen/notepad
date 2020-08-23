1f0d3e9a98553ca30c307b2eea9910079eadcea6
Foxegen <foxegen55@gmail.com>
2020-08-23 20:22:56 +0300

    Updated output table

--- read.rb
+++ read.rb
@@ -39,14 +39,129 @@ if result.is_a? Post
   end
 
 else # покажем таблицу результатов
+  id_size = 0
+  text_size = 0
+  url_size = 0
+  due_date_size = false
 
-  print "| id\t| @type\t|  @created_at\t\t\t|  @text \t\t\t| @url\t\t| @due_date \t "
+  # Наполняем переменные size
+  result.each do |row|
+    if row[0].size > id_size
+      id_size = row[0].size
+    end
 
+    if !row[3].nil? && row[3].split("\n").join(" ").size > text_size
+      text_size = row[3].split("\n").join(" ").size
+    end
+
+    if !row[4].nil? && row[4].size > url_size
+      url_size = row[4].size
+    end
+
+    unless row[5].nil?
+      due_date_size = true
+    end
+  end
+
+  # Ограничение для text_size
+  text_size = 40 if text_size > 40
+
+  # Выводим поля таблицы
+  print "| id " + " "*(id_size - 4) + "| @type |  @created_at" + " "* 14
+  print "|" if text_size == 0 && url_size == 0 && !due_date_size
+
+  # Исключаем отсутвие поля @text
+  unless text_size == 0
+
+    if text_size < 5
+      print "| @text "
+
+      # Добавляем "|", если после поля @text нет других полей
+      print "|" if url_size == 0 && !due_date_size
+    else
+      print "| @text " + " "*(text_size - 5)
+
+      # Добавляем "|", если после поля @text нет других полей
+      print "|" if url_size == 0 && !due_date_size
+    end
+  end
+
+  # Исключаем отсутвие поля @url
+  unless url_size == 0
+
+    if url_size < 4
+      print "| @url "
+
+      # Добавляем "|", если после поля @url нет других полей
+      print "|" unless due_date_size
+    else
+      print "| @url " + " "*(url_size - 4)
+
+      # Добавляем "|", если после поля @url нет других полей
+      print "|" unless due_date_size
+    end
+  end
+
+  # Исключаем отсутствие @due_date
+  if due_date_size
+    print "| @due_date  |"
+  end
+
+
+  # Выводим данные таблицы
   result.each do |row|
     puts
 
-    row.each do |element|
-      print "| #{element.to_s.delete("\n")[0..40]}\t"
+    print "| #{row[0]}  |"
+    print " #{row[1]}  |"
+    print " #{row[2]} |"
+
+    # Исключаем отсутствие @text
+    unless text_size == 0
+
+      # Выводим значение, когда отсутствует поле text у row и все поля text меньше, чем заголовок @text
+      if row[3].nil? && text_size < 5
+        print " "* 7 + "|"
+      # Выводим значение, когда все поля text меньше, чем заголовок @text
+      elsif text_size < 5
+        print " #{row[3].split("\n").join(" ")} " + " "*(5 - row[3].size) + "|"
+      # Выводим значение, когда отсутствует поле text у row
+      elsif row[3].nil?
+        print "  " + " "*text_size + "|"
+      # Выводим значение, когда хотя бы одно поле text больше, чем заголовок @text или равно ему
+      else
+        print " #{row[3].split("\n").join(" ")[0..40]} " + " "*(text_size - row[3].size) + "|"
+      end
+    end
+
+    # Исключаем отсутствие @url
+    unless url_size == 0
+
+      # Выводим значение, когда отсутствует поле url у row и все поля url меньше, чем заголовок @url
+      if row[4].nil? && url_size < 4
+        print " "* 6 + "|"
+      # Выводим значение, когда все поля url меньше, чем заголовок @url
+      elsif url_size < 4
+        print " #{row[4]} " + " "*(4 - row[4].size) + "|"
+      # Выводим значение, когда отсутствует поле url у row
+      elsif row[4].nil?
+        print "  " + " "*url_size + "|"
+      # Выводим значение, когда хотя бы одно поле url больше, чем заголовок @url или равно ему
+      else
+        print " #{row[4]} " + " "*(url_size - row[4].size) + "|"
+      end
+    end
+
+    # Исключаем отсутствие @due_date
+    if due_date_size
+
+      # Выводим значение, когда отсутствует поле due_date у row
+      if row[5].nil?
+        print " "* 12 + "|"
+      # Выводим поле due_date
+      else
+        print " #{row[5]} |"
+      end
     end
   end
 end
